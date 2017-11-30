594b0220effba27b6947fa69220cd166d36631f0
Unknown <gabriel.miro@epitech.eu>
2017-11-30 15:58:32 +0900

    dateStamp ok

--- Projet1/Source.cpp
+++ Projet1/Source.cpp
@@ -1,10 +1,14 @@
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-
+#include <chrono>
+#include <iomanip>
+#include <iostream>
+#pragma warning(disable : 4996)
 using namespace cv;
 using namespace std;
 
@@ -51,9 +55,138 @@ void addNoise(Mat image)
 
 }
 
-void addTimeStamp(Mat image)
+void addTimeStamp(Mat image, Mat imageC)
 {
+	time_t t = time(0);
+	struct tm * now = localtime(&t);
 
+	int day = now->tm_mday;
+	int month = now->tm_mon + 1;
+	int year = now->tm_year + 1900;
+	int hour = now->tm_hour;
+	int min = now->tm_min;
+	int size = 0;
+	int size2 = 0;
+	//PM OR AM
+	Mat M = imread("pm.png", IMREAD_COLOR);
+	if (hour > 12) {
+		hour = hour - 12;
+		M = imread("pm.png", IMREAD_COLOR);
+		cv::resize(M, M, M.size() / 3);
+		M.copyTo(image(cv::Rect(30, image.size().height - 80, M.cols, M.rows)));
+	}
+	else {
+		M = imread("am.png", IMREAD_COLOR);
+		cv::resize(M, M, M.size() / 3);
+		M.copyTo(image(cv::Rect(30, image.size().height - 80, M.cols, M.rows)));
+	}
+	//HOUR
+	if (hour < 10) {
+		string hh = std::to_string(hour) + ".png";
+		Mat H = imread(hh, IMREAD_COLOR);
+		cv::resize(H, H, H.size() / 3);
+		H.copyTo(image(cv::Rect(M.size().width + 45, image.size().height - 80, H.cols, H.rows)));
+		size = M.size().width + 45 + H.size().width;
+	}
+	else {
+		Mat H = imread("1.png", IMREAD_COLOR);
+		cv::resize(H, H, H.size() / 3);
+		H.copyTo(image(cv::Rect(M.size().width + 45, image.size().height - 80, H.cols, H.rows)));
+		hour = hour - 10;
+		string hh = std::to_string(hour) + ".png";
+		H = imread(hh, IMREAD_COLOR);
+		cv::resize(H, H, H.size() / 3);
+		H.copyTo(image(cv::Rect(M.size().width + 45 + H.size().width, image.size().height - 80, H.cols, H.rows)));
+		size = M.size().width + 40 + H.size().width + H.size().width;
+	}
+	Mat pts = imread("2pts.png", IMREAD_COLOR);
+	cv::resize(pts, pts, pts.size() / 3);
+	pts.copyTo(image(cv::Rect(size + 5, image.size().height - 75, pts.cols, pts.rows)));
+	size = size + pts.size().width;
+	//MIN
+	std::string s = std::to_string(min);
+	char const *pchar = s.c_str();
+	int i = pchar[0] - 48;
+	string dec = std::to_string(i) + ".png";
+	Mat DEC = imread(dec, IMREAD_COLOR);
+	cv::resize(DEC, DEC, DEC.size() / 3);
+	DEC.copyTo(image(cv::Rect(size + 10, image.size().height - 80, DEC.cols, DEC.rows)));
+	size = DEC.size().width + size;
+	if (min > 9) {
+		i = pchar[1] - 48;
+		dec = std::to_string(i) + ".png";
+		Mat MIN = imread(dec, IMREAD_COLOR);
+		cv::resize(MIN, MIN, MIN.size() / 3);
+		MIN.copyTo(image(cv::Rect(size + 12, image.size().height - 80, MIN.cols, MIN.rows)));
+		size = MIN.size().width + size;
+	}
+	//MONTH
+	string mm = std::to_string(month) + "m.png";
+	M = imread(mm, IMREAD_COLOR);
+	cv::resize(M, M, M.size() / 3);
+	M.copyTo(image(cv::Rect(30, image.size().height - 50, M.cols, M.rows)));
+	size2 = M.size().width + 43;
+	//DAY
+	s = std::to_string(day);
+	pchar = s.c_str();
+	i = pchar[0] - 48;
+	dec = std::to_string(i) + ".png";
+	DEC = imread(dec, IMREAD_COLOR);
+	cv::resize(DEC, DEC, DEC.size() / 3);
+	DEC.copyTo(image(cv::Rect(size2, image.size().height - 50, DEC.cols, DEC.rows)));
+	size2 = DEC.size().width + size2;
+	if (day > 9) {
+		i = pchar[1] - 48;
+		dec = std::to_string(i) + ".png";
+		DEC = imread(dec, IMREAD_COLOR);
+		cv::resize(DEC, DEC, DEC.size() / 3);
+		DEC.copyTo(image(cv::Rect(size2 + 3, image.size().height - 50, DEC.cols, DEC.rows)));
+		size2 = DEC.size().width + size2 + 20;
+	}
+
+	//YEAR
+	s = std::to_string(year);
+	pchar = s.c_str();
+	i = pchar[0] - 48;
+	dec = std::to_string(i) + ".png";
+	DEC = imread(dec, IMREAD_COLOR);
+	cv::resize(DEC, DEC, DEC.size() / 3);
+	DEC.copyTo(image(cv::Rect(size2, image.size().height - 50, DEC.cols, DEC.rows)));
+	size2 = DEC.size().width + size2 + 3;
+	i = pchar[1] - 48;
+	dec = std::to_string(i) + ".png";
+	DEC = imread(dec, IMREAD_COLOR);
+	cv::resize(DEC, DEC, DEC.size() / 3);
+	DEC.copyTo(image(cv::Rect(size2, image.size().height - 50, DEC.cols, DEC.rows)));
+	size2 = DEC.size().width + size2 + 3;
+	i = pchar[2] - 48;
+	dec = std::to_string(i) + ".png";
+	DEC = imread(dec, IMREAD_COLOR);
+	cv::resize(DEC, DEC, DEC.size() / 3);
+	DEC.copyTo(image(cv::Rect(size2, image.size().height - 50, DEC.cols, DEC.rows)));
+	size2 = DEC.size().width + size2 + 3;
+	i = pchar[3] - 48;
+	dec = std::to_string(i) + ".png";
+	DEC = imread(dec, IMREAD_COLOR);
+	cv::resize(DEC, DEC, DEC.size() / 3);
+	DEC.copyTo(image(cv::Rect(size2, image.size().height - 50, DEC.cols, DEC.rows)));
+	size2 = DEC.size().width + size2 + 3;
+
+
+	for (int y = image.size().height - 80; y <image.size().height - 20; y++)
+	{
+		for (int x = 30; x< size2; x++)
+		{
+			Vec3b color = image.at<Vec3b>(Point(x, y));
+			Vec3b colorC = imageC.at<Vec3b>(Point(x, y));
+			if (color[0] < 20 && color[1] < 20 && color[2] < 20) {
+				color[0] = colorC[0];
+				color[1] = colorC[1];
+				color[2] = colorC[2];
+			}
+			image.at<Vec3b>(Point(x, y)) = color;
+		}
+	}
 }
 
 int main(int argc, char** argv)
@@ -71,12 +204,14 @@ int main(int argc, char** argv)
 		cout << "Could not open or find the image" << std::endl;
 		return -1;
 	}
+	Mat imageC;
+	imageC = imread(argv[1], IMREAD_COLOR);
 
 	//todo
 	stretchPixels(image);
 	distortColors(image);
 	addNoise(image);
-	addTimeStamp(image);
+	addTimeStamp(image, imageC);
 
 	namedWindow("Display window", WINDOW_AUTOSIZE); // Create a window for display.
 	imshow("Display window", image); // Show our image inside it.
